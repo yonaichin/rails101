e0d920f765e93d2ec03ff2d12b58fbebb3bc9db3
Naichin <yonaichin@gmail.com>
2014-11-25 17:39:30 +0800

    user 可以加入、退出 group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,10 +20,11 @@ class GroupsController < ApplicationController
     @group = current_user.groups.find(params[:id])
   end
 
-  def create
-     @group = current_user.groups.new(group_params)
+    def create
+    @group = current_user.groups.new(group_params)
   
     if @group.save 
+      current_user.join!(@group)
       redirect_to groups_path, :notice => '新增討論版成功'
     else
       render :new
@@ -46,11 +47,38 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, :alert => '討論版已刪除'
   end
+   def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本討論版成功！"
+    else
+      flash[:warning] = "你已經是本討論版成員了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本討論版！"
+    else
+      flash[:warning] = "你不是本討論版成員，怎麼退出 XD"
+    end
+
+    redirect_to group_path(@group)
+  end
 
   private
   
   def group_params
       params.require(:group).permit(:title, :description)
   end
+
+  
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  
+
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
+
     resources :posts
   end
 
